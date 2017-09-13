9abc1aa49b49d2bf63cdcc83777a3702d2b7078a
stevenanhnguyen <at.mrsteven@gmail.com>
2017-09-13 21:32:05 +0700

    add view devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :feedbacks, dependent: :destroy
   has_many :orders, dependent: :destroy
   validates_associated :feedbacks
